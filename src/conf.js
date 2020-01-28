51d1b8dc30a4548d5d76aa3dd7f703e30e5cb37e
Shubham Kulkarni <58355613+shubhamsk894@users.noreply.github.com>
2020-01-28 14:39:33 +0530

    multiple symbols

--- src/conf.js
+++ src/conf.js
@@ -1,4 +1,4 @@
-const currencyRates = `https://min-api.cryptocompare.com/data/price?api_key=10794c4591cc070f1c396a65eadb80b422f838b874385337d1097cecb41229c2`;
+const currencyRates = `https://min-api.cryptocompare.com/data/pricemulti?api_key=10794c4591cc070f1c396a65eadb80b422f838b874385337d1097cecb41229c2`;
 
 module.exports = {
   currencyRates
